cab86e431b5e143c2e30d624ff37d27a45da2556
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 19:51:18 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func footnote() {
+  fmt.Println("Copyright c 2020 by Nicholas Larsen")
+  fmt.Println("All rights reserved. No parts of this code may be reproduced or used in any manner without written permission of the copyright owner except for the use of quotations in a code review")
+}
+func main() {
+  footnote()
+}
