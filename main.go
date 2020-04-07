03fe9dbb2c322f7d7e3976a57b0313322b2fb478
nlarsen7 <nlarsen2013@gmail.com>
2020-04-07 19:53:37 +0000

    Footnote 3.2.2

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/7/2020
+//a copyright function printed out
 package main
 
 import "fmt"
@@ -5,7 +8,9 @@ import "fmt"
 func footnote() {
   fmt.Println("Copyright c 2020 by Nicholas Larsen")
   fmt.Println("All rights reserved. No parts of this code may be reproduced or used in any manner without written permission of the copyright owner except for the use of quotations in a code review")
+  //the copyright message under the footnote function
 }
 func main() {
   footnote()
+  //displays what was done in the footnote function
 }
